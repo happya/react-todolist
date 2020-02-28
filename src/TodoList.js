ededb612a29dfcb30b67b2a210fc35c28874343e
yyi <yyi@uw.edu>
2020-02-27 22:17:30 -0800

    init

---
+++ src/TodoList.js
@@ -0,0 +1,35 @@
+import React, { Component, Fragment } from "react";
+
+class TodoList extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+
+    }
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <input
+                        value={this.state.inputValue}
+                        onChange={this.handleInputChange.bind(this)}
+                    />
+                    <button>submit</button>
+                </div>
+                <ul>
+                    <li>React</li>
+                </ul>
+            </Fragment>
+        )
+    }
+    handleInputChange(e){
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+}
+
+export default TodoList;
