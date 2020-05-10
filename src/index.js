eb463da6f2413c80ad30a8b3156397c31c73bfd8
happya <whuyaya@gmail.com>
2020-05-10 01:15:30 -0700

    add features of redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList2 from './TodoList2';
+// import TodoList2 from './TodoList2';
 // import App from './App'
-
-ReactDOM.render(<TodoList2 />, document.getElementById('root'));
+import TodoListRd from './TodoListRd'
+ReactDOM.render(<TodoListRd />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
